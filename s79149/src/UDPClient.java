d9c74d462c80de7fceac88628d3b30044dbb3c78
Mauritius Berger <mauritius.berger@t-online.de>
2019-12-03 10:44:38 +0100

    add latest changes

--- s79149/src/UDPClient.java
+++ s79149/src/UDPClient.java
@@ -1,41 +1,42 @@
 import java.io.*;
 import java.net.*;
 import java.util.Random;
+import java.util.zip.CRC32;
 import java.nio.charset.Charset;
 import java.nio.ByteBuffer;
 
 class UDPClient {
-	private static byte[] session_number = new byte[2];
+	// constants
+	private static final Integer MAX_REPEATS = 10;
+
+	private static variable send_session_number = new variable(new byte[2], 2);
+
 	// does not need to be an array, but it's simpler for further calculations, if it is
-	private static byte[] packet_number = new byte[1];
-	private static byte[] keyword = new byte[5];
-	private static byte[] file_length = new byte[8]; // uint_64 = Long.Bytes
-	private static byte[] file_name_length = new byte[2]; // uint_16
-	private static byte[] file_name; // variable from 0 to 255 // TODO: needs to get a check
-	private static byte[] check_sum_CRC32 = new byte[4];
+	private static variable send_packet_number = new variable(new byte[1], 1);
+	
+	// start packet
+	private static variable send_keyword = new variable(new byte[5], 5);
+	private static variable send_file_length = new variable(new byte[8], 8); // uint_64 = Long.Bytes
+	private static variable send_file_name_length = new variable(new byte[2], 2); // uint_16
+	private static variable send_file_name = new variable(new byte[0], 0); // variable from 0 to 255 // TODO: needs to get a check
 
-	private static byte[] sendData;
+	// non-start packet
+	private static variable send_file_data = new variable(new byte[512], 512);
 
-	/**
-	 * CRC32
-	 */
-	public static class CRC32 {
-		private static byte[] checksum;
+	private static variable send_check_sum_CRC32 = new variable(new byte[4], 4);
 
-		public byte[] getChecksum()
-		{
-			return checksum;
-		}
+	private static variable send_data = new variable(new byte[0], 0);
 
-		/**
-		 * Calculates Checksum
-		 * @param data data, which wants to get checked
-		 */
-		public void calcChecksum(byte[] data, Integer length)
-		{
-			/* TODO: add algorithm */
-		}
-	}
+
+	private static variable recv_session_number = new variable(new byte[2], 2);
+	private static variable recv_packet_number = new variable(new byte[1], 1);
+
+	private static variable recv_data = new variable(new byte[1024], 1024); // TODO: change to variable
+
+
+	private static File file;
+	private static Integer port;
+
 
 	private static void checkParamter(String args[])
 	{
@@ -70,40 +71,104 @@ class UDPClient {
 
 	private static void mergeSendData()
 	{
-		sendData = new byte[
-			session_number.length
-			+ packet_number.length
-			+ keyword.length 
-			+ file_length.length
-			+ file_name_length.length
-			+ file_name.length
-			+ check_sum_CRC32.length
-		];
-		Integer currentSendDataLength = 0;
-		// https://www.javatpoint.com/how-to-merge-two-arrays-in-java
-		System.arraycopy(session_number, 0, sendData, currentSendDataLength, session_number.length);
-		currentSendDataLength += session_number.length;  
-		System.arraycopy(packet_number, 0, sendData, currentSendDataLength, packet_number.length);
-		currentSendDataLength += packet_number.length;  
-		System.arraycopy(keyword, 0, sendData, currentSendDataLength, keyword.length);
-		currentSendDataLength += keyword.length;
-		System.arraycopy(file_length, 0, sendData, currentSendDataLength, file_length.length);
-		currentSendDataLength += file_length.length;
-		System.arraycopy(file_name_length, 0, sendData, currentSendDataLength, file_name_length.length);
-		currentSendDataLength += file_name_length.length;
-		System.arraycopy(file_name, 0, sendData, currentSendDataLength, file_name.length);
+		send_data.append(send_session_number);
+		send_data.append(send_packet_number);
+
+		// start packet
+		send_data.append(send_keyword);
+		send_data.append(send_file_length);
+		send_data.append(send_file_name_length);
+		send_data.append(send_file_name);
+
+		// non-start packet
+		send_data.append(send_file_data);
 	}
 	
 	private static void addCRC32()
 	{
+		System.out.println("Size of send_data: " + send_data.length);
+
 		CRC32 checksum = new CRC32();
-		checksum.calcChecksum(sendData, sendData.length - check_sum_CRC32.length);
-		check_sum_CRC32 = checksum.getChecksum();
-	
-		System.arraycopy(check_sum_CRC32, 0, sendData, sendData.length - check_sum_CRC32.length, check_sum_CRC32.length);
+		byte[] newArr = new byte[send_data.length - send_check_sum_CRC32.length];
+
+		System.out.println("Size of newArr: " + newArr.length);
+
+		System.arraycopy(send_data, 0, newArr, 0, send_data.length - send_check_sum_CRC32.length);
+		checksum.update(newArr);
+		send_data = newArr;
+
+		// https://stackoverflow.com/questions/4485128/how-do-i-convert-long-to-byte-and-back-in-java
+		ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+		buffer.putLong(checksum.getValue());
+
+		System.arraycopy(buffer.array(), buffer.array().length - send_check_sum_CRC32.length, send_check_sum_CRC32, 0, buffer.array().length - send_check_sum_CRC32.length);
+
+		System.out.println("Checksum: " +  checksum.getValue());
+
+		for (int i = 0; i < send_check_sum_CRC32.length; i++) {
+			System.out.format("SumVar: %x\n", send_check_sum_CRC32[i]);
+		}
+
+		newArr = new byte[send_data.length + send_check_sum_CRC32.length];
+		System.arraycopy(send_data, 0, newArr, 0, send_data.length - send_check_sum_CRC32.length);
+		send_data = newArr;
+
+		if (send_check_sum_CRC32 != null) {
+			System.arraycopy(send_check_sum_CRC32, 0, send_data, send_data.length - send_check_sum_CRC32.length, send_check_sum_CRC32.length);
+		}
+
+		System.out.println("Size of send_data: " + send_data.length);
+
+		checksum.reset();
 	}
 
-	private static void sendFile(String hostname, File file) throws Exception
+	private static void configureStartPacket()
+	{
+		send_session_number.random();
+
+		System.out.println("Session number: " + send_session_number.getShort());
+
+		// TODO: add byte[] buf = {(byte) 0};
+		send_packet_number[0] = (byte) 0;
+		// https://stackoverflow.com/questions/18571223/how-to-convert-java-string-into-byte
+		send_keyword = "Start".getBytes(Charset.forName("US-ASCII"));
+
+		// https://stackoverflow.com/questions/4485128/how-do-i-convert-long-to-byte-and-back-in-java
+		ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES); // 8 Bytes = 64 bit
+		buffer.putLong(file.length());
+		send_file_length = buffer.array();
+		
+		buffer = ByteBuffer.allocate(2); // 2 Byte = 16 bit
+		buffer.putShort((short) file.getName().length());
+		send_file_name_length = buffer.array();
+
+		send_file_name = file.getName().getBytes(Charset.forName("UTF-8"));
+	}
+
+	private static void configureDataPacket()
+	{
+		// TODO: set "old" data to null
+		// send_session_number is const
+		send_packet_number[0] = (byte) ((send_packet_number[0] + 1) % 2);
+
+		// start_packet data
+		// TODO: add bools and change current vars to send_..._data or ..._value
+		send_keyword = new byte[0];
+		send_file_length = new byte[0];
+		send_file_name_length = new byte[0];
+		send_file_name = new byte[0];
+
+		// TODO: add file data
+	}
+
+	private static void configureLastPacket()
+	{
+		send_packet_number[0] = (byte) ((send_packet_number[0] + 1) % 2);
+
+		// TODO: add end of file data
+	}
+
+	private static void sendFile(String hostname) throws Exception
 	{
 		Integer timeout = 1000; // timeout in millisecs // TODO: set real timeout
 		DatagramSocket clientSocket = new DatagramSocket();
@@ -111,66 +176,52 @@ class UDPClient {
 
 		InetAddress IPAddress = InetAddress.getByName(hostname);
 
-		byte[] receiveData = new byte[1024]; // TODO: set fix size
-
-
-		// packet_start
-		Random random = new Random();
-		random.nextBytes(session_number);
-		packet_number[0] = (byte) 0;
-		// https://stackoverflow.com/questions/18571223/how-to-convert-java-string-into-byte
-		keyword = "Start".getBytes(Charset.forName("UTF-8"));
-
-		// https://stackoverflow.com/questions/4485128/how-do-i-convert-long-to-byte-and-back-in-java
-		ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES); // 8 Bytes = 64 bit
-		buffer.putLong(file.length());
-		file_length = buffer.array();
-		
-		buffer = ByteBuffer.allocate(2); // 2 Byte = 16 bit
-		buffer.putLong(file.getName().length());
-		file_name_length = buffer.array();
-
-		file_name = file.getName().getBytes(Charset.forName("UTF-8"));
-
-
-		// at the end
-		mergeSendData();
-		addCRC32();
-
-		/*
-		for (int seq = 0; seq<MAX_PINGS; seq++) {
-			long time_send = System.currentTimeMillis();
-			String sentence = "PING " + seq + " " + time_send + " \r\n";
-
-			sendData = sentence.getBytes();
-				DatagramPacket sendPacket =
-			new DatagramPacket(sendData, sendData.length, IPAddress, port);
-
-			clientSocket.send(sendPacket);
-
-			DatagramPacket receivePacket =
-			new DatagramPacket(receiveData, receiveData.length);
-
-			clientSocket.receive(receivePacket);
-
-			if (receivePacket.getData() == null) {
-				// Packetverlust
+		Integer packet_num = 3; // TODO: calculate from size of file
+		for (Integer i = 0; i < packet_num; i++) {
+			if (i == 0) { // start packet
+				configureStartPacket();
+				mergeSendData();
+				addCRC32();
+			} else if (i == packet_num - 1) { // evtl. packet_num
+				configureLastPacket();
+				mergeSendData();
+				addCRC32(); // TODO: over complete file!!
+			} else {
+				configureDataPacket();
+				mergeSendData();
 			}
-			else {
-				String receiveSentence =
-				new String(receivePacket.getData());
-				String[] receiveArgs = receiveSentence.split(" ");
+			
+			Integer seq;
+			for (seq = 0; seq<MAX_REPEATS; seq++) {
+				System.out.println((byte) send_packet_number[0]);
+				// long time_send = System.currentTimeMillis();
+	
+				DatagramPacket send_packet =
+				 new DatagramPacket(send_data, send_data.length, IPAddress, port);
+				clientSocket.send(send_packet);
 
-				if (receiveArgs.length != 4) {
-					System.out.println("Unknown Receive. Length " + receiveArgs.length + " != 4!");
+				DatagramPacket recv_packet =
+				 new DatagramPacket(recv_data, recv_data.length);
+	
+				try {
+					clientSocket.receive(recv_packet);
+					if (recv_packet.getData() == null) {
+						throw new Exception("Receive Packet data is null");
+					}
+					else {
+						System.out.println("Recv data: " + recv_packet.getData());
+						break;
+					}
+				} catch (java.net.SocketTimeoutException e) {
+					System.out.println("No answer from Server");
+				} catch (Exception e) {
+					throw new Exception(e);
 				}
-				else {
-					long time_diff = System.currentTimeMillis() - Long.parseLong(receiveArgs[2]);
-					System.out.println("Time diff: " + time_diff);
-				}
+			}
+			if (seq == MAX_REPEATS) {
+				throw new Exception("Got no answer after 10 retries");
 			}
 		}
-		*/
 
 		// TODO: Auswertung der Zeiten
 
@@ -182,12 +233,12 @@ class UDPClient {
 		checkParamter(args);
 
 		String hostname = args[0];
-		int port = Integer.parseInt(args[1]);
+		port = Integer.parseInt(args[1]);
 		String file_name = args[2];
 
 		try {
-			File file = getFile(file_name);
-			sendFile(hostname, file);
+			file = getFile(file_name);
+			sendFile(hostname);
 		} catch (Exception e) {
 			throw new Exception(e);
 		}
--- s79149/src/UDPServer.java
+++ s79149/src/UDPServer.java
@@ -1,7 +1,33 @@
 import java.io.*;
 import java.net.*;
+import java.nio.ByteBuffer;
 
 class UDPServer {
+	// --- variables -------------------
+	private static byte[] recv_session_number = new byte[2]; // random uint_16
+	// does not need to be an array, but it's simpler for further calculations, if it is
+	private static byte[] recv_packet_number = new byte[1]; // 0 or 1
+	
+	// start packet
+	private static byte[] recv_keyword = new byte[5]; // String 5
+	private static byte[] recv_file_length = new byte[8]; // uint_64 = Long.Bytes
+	private static byte[] recv_file_name_length = new byte[2]; // uint_16
+	private static byte[] recv_file_name = new byte[0]; // variable from 0 to 255 // TODO: needs to get a check
+
+	// non-start packet
+	private static byte[] recv_file_data = new byte[512];
+
+	private static byte[] recv_check_sum_CRC32 = new byte[4];
+
+	private static byte[] recv_data = new byte[1024]; // TODO: calculate
+
+
+	private static byte[] send_session_number = new byte[2];
+	private static byte[] send_packet_number = new byte[1];
+
+	private static byte[] send_data = new byte[1024]; // TODO: change to variable
+
+
 	private static void checkParameter(String args[])
 	{
 		if (args.length != 1) {
@@ -13,14 +39,141 @@ class UDPServer {
 		// TODO: check parameter if it is an integer and if it is a valid port etc.
 	}
 
+	private static void printData()
+	{
+		Integer currentRecvDataLength = 0;
+		// https://www.javatpoint.com/how-to-merge-two-arrays-in-java
+		System.arraycopy(recv_data, 0, recv_session_number, currentRecvDataLength, recv_session_number.length);
+		currentRecvDataLength += recv_session_number.length;
+		System.arraycopy(recv_data, currentRecvDataLength, recv_packet_number, 0, recv_packet_number.length);
+		currentRecvDataLength += recv_packet_number.length;
+
+		// start packet
+		System.arraycopy(recv_data, currentRecvDataLength, recv_keyword, 0, recv_keyword.length);
+		currentRecvDataLength += recv_keyword.length;
+		System.arraycopy(recv_data, currentRecvDataLength, recv_file_length, 0, recv_file_length.length);
+		currentRecvDataLength += recv_file_length.length;
+		System.arraycopy(recv_data, currentRecvDataLength, recv_file_name_length, 0, recv_file_name_length.length);
+		currentRecvDataLength += recv_file_name_length.length;
+
+		// TODO: add variable recv_file_name!!!
+		// recv_file_name = new byte[];
+
+		System.arraycopy(recv_data, currentRecvDataLength, recv_file_name, 0, recv_file_name.length);
+		currentRecvDataLength += recv_file_name.length;
+
+		// non-start packet
+		System.arraycopy(recv_data, currentRecvDataLength, recv_file_data, 0, recv_file_data.length);
+		currentRecvDataLength += recv_file_data.length;
+		// currently throws an exception, because the recv_file_data size needs to get calculated
+		 
+		// last packet
+		// TODO: rm 512
+		System.arraycopy(recv_data, currentRecvDataLength, recv_check_sum_CRC32, 0, recv_check_sum_CRC32.length);
+		currentRecvDataLength += recv_check_sum_CRC32.length;
+
+		System.out.println("recv_length: " + recv_data.length);
+		System.out.println("curr recv length: " + currentRecvDataLength);
+
+
+
+		// https://stackoverflow.com/questions/4485128/how-do-i-convert-long-to-byte-and-back-in-java
+		ByteBuffer buffer = ByteBuffer.allocate(2);
+		buffer.put(recv_session_number);
+		buffer.flip(); //need flip 
+		System.out.println("Buffer size: " + buffer.array().length);
+		System.out.println("Session number: " + buffer.getShort());
+
+		// https://stackoverflow.com/questions/4485128/how-do-i-convert-long-to-byte-and-back-in-java
+		buffer = ByteBuffer.allocate(1);
+		buffer.put(recv_packet_number);
+		buffer.flip(); //need flip
+		System.out.println("Buffer size: " + buffer.array().length);
+		System.out.println(String.format("Packet number: %d", buffer.array()[0]));
+
+		// https://stackoverflow.com/questions/4485128/how-do-i-convert-long-to-byte-and-back-in-java
+		buffer = ByteBuffer.allocate(5);
+		buffer.put(recv_keyword);
+		buffer.flip(); //need flip
+
+		// sets encoding to US-ASCII
+		// System.setProperty("file.encoding", "US-ASCII");
+
+		System.out.println("Buffer size: " + buffer.array().length);
+		System.out.println(String.format("keyword: %c", buffer.getChar()));
+
+		// https://stackoverflow.com/questions/4485128/how-do-i-convert-long-to-byte-and-back-in-java
+		buffer = ByteBuffer.allocate(Long.BYTES);
+		buffer.put(recv_file_length);
+		buffer.flip(); //need flip
+		System.out.println("Buffer size: " + buffer.array().length);
+		System.out.println("file length: " + buffer.getLong());
+
+		// https://stackoverflow.com/questions/4485128/how-do-i-convert-long-to-byte-and-back-in-java
+		buffer = ByteBuffer.allocate(4);
+		buffer.put(recv_check_sum_CRC32);
+		buffer.flip(); //need flip
+		System.out.println("Buffer size: " + buffer.array().length);
+		System.out.println("CRC32: " + buffer.getInt());
+
+		// TODO: get last packet durch erreichen der file laenge
+
+
+		for (int i = 0; i < recv_data.length; i++) {
+			if (recv_data[i] != 0)
+				System.out.format("i: %d - %x\n", i, recv_data[i]);
+		}
+	}
+
 	/**
 	 * Gets file from client
 	 * @param port port, where to listen
 	 * @return String filename
 	 * @throws Exception
 	 */
-	private static String getFile(int port) throws Exception
+	private static String recvFile(int port) throws Exception
 	{
+		 DatagramSocket socket = new DatagramSocket(port);
+		 socket.setSoTimeout(10000);
+
+		long time_send = System.currentTimeMillis();
+		for (int j=0; true; j++) {
+			// TODO: change to the calculated maximum
+			DatagramPacket request = new DatagramPacket(new byte[1024], 1024);
+
+			try {
+				socket.receive(request);
+			} catch (java.net.SocketTimeoutException e) {
+				System.out.println("Got timeout, exit.");
+				socket.close();
+				System.exit(1);
+			} catch (Exception e) {
+				throw e;
+			}
+
+			recv_data = request.getData();
+			
+			printData();
+
+			// send_data = ...
+   
+			InetAddress clientHost = request.getAddress();
+			int clientPort = request.getPort();
+			DatagramPacket reply = new DatagramPacket(send_data, send_data.length, clientHost, clientPort);
+			socket.send(reply);
+   
+			System.out.println("Reply sent " + j);
+
+			System.out.println(System.currentTimeMillis());
+			System.out.println(time_send);
+
+			if (System.currentTimeMillis() - time_send > 5000) {
+				break;
+			}
+		}
+
+		socket.close();
+
 		return "file.file";
 	}
 
@@ -43,6 +196,7 @@ class UDPServer {
 		}
 
 		// now the filename is avaiable.
+		System.out.println(filename + " is avaiable.");
 
 		return;
 	}
@@ -54,7 +208,7 @@ class UDPServer {
 		String filename;
 
 		try {
-			filename = getFile(port);
+			filename = recvFile(port);
 			saveFile(filename);
 		} catch (Exception e) {
 			throw new Exception(e);
